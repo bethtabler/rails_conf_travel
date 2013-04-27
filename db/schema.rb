d8ee46761eae1dfa68c1b6256eb84ca7c58708fc
Noel Rappin <noel@groupon.com>
2013-04-26 23:31:45 -0500

    adding field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130417151237) do
+ActiveRecord::Schema.define(:version => 20130427043103) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trip_id"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(:version => 20130417151237) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.integer  "home_year"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
