57cf1d50c33588b681637735fb37fa63c72cebe4
Noel Rappin <noel@groupon.com>
2013-04-30 07:08:56 -0700

    commenting tests out

--- spec/models/hotel_stay_spec.rb
+++ spec/models/hotel_stay_spec.rb
@@ -2,23 +2,23 @@ require "spec_helper"
 
 describe HotelStay do
 
-  describe "price" do
-    it "calculates its own price" do
-      hotel = FactoryGirl.create(:hotel, price: 50)
-      stay = HotelStay.new(hotel, 3)
-      expect(stay.pre_tax_total).to eq(150)
-    end
-  end
+  # describe "price" do
+  #   it "calculates its own price" do
+  #     hotel = # how do I create a hotel here?
+  #     stay = HotelStay.new(hotel, 3)
+  #     expect(stay.pre_tax_total).to eq(150)
+  #   end
+  # end
 
-  describe "availability" do
-    it "should description" do
-      hotel = FactoryGirl.build(:hotel)
-      stay = HotelStay.new(hotel, 3, "Jan 21, 1971")
-      stay.availablity_checker.should_receive(:available_for_range)
-          .with(Date.parse("Jan 21, 1971"), 3)
-          .and_return(true)
-      expect(stay.available?).to be_true
-    end
-  end
+  # describe "availability" do
+  #   it "should description" do
+  #     hotel = # how to create a hotel here
+  #     stay = HotelStay.new(hotel, 3, "Jan 21, 1971")
+  #
+  #     # What do I stub here?
+  #
+  #     expect(stay.available?).to be_true
+  #   end
+  # end
 
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -2,25 +2,25 @@ require 'spec_helper'
 
 describe Purchase do
 
-  describe "pricing" do
-    let(:trip) { FactoryGirl.create(:trip, price: 200) }
+  # describe "pricing" do
+  #   let(:trip) { FactoryGirl.create(:trip, price: 200) }
 
-    it "can calculate price given a trip" do
-      purchase = Purchase.new(trip)
-      expect(purchase.pre_tax_total).to eq(200)
-    end
+  #   it "can calculate price given a trip" do
+  #     purchase = # how to create a purchase here
+  #     expect(purchase.pre_tax_total).to eq(200)
+  #   end
 
-    it "can calculate price given a trip and a hotel stay" do
-      hotel_stay = stub(pre_tax_total: 150)
-      purchase = Purchase.new(trip, hotel_stay)
-      expect(purchase.pre_tax_total).to eq(350)
-    end
+  #   it "can calculate price given a trip and a hotel stay" do
+  #     hotel_stay = # what is a hotel stay
+  #     purchase = # how to create a purchase
+  #     expect(purchase.pre_tax_total).to eq(350)
+  #   end
 
-    it "can calculate a price given some activities " do
-      cheap_activity = Activity.new(price: 50)
-      expensive_activity = Activity.new(price: 1000)
-      purchase = Purchase.new(trip, nil, [cheap_activity, expensive_activity])
-      expect(purchase.pre_tax_total).to eq(200 + 50 + 1000)
-    end
-  end
+  #   it "can calculate a price given some activities " do
+  #     cheap_activity = # how do I create an activity
+  #     expensive_activity = # And again...
+  #     purchase = # what api do I want for purchase
+  #     expect(purchase.pre_tax_total).to eq(200 + 50 + 1000)
+  #   end
+  # end
 end
