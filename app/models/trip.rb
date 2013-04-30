1224ee798c06769ad6a6c411336241ba249345f6
Noel Rappin <noel@groupon.com>
2013-04-30 07:04:38 -0700

    last batch

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,8 @@ class Trip < ActiveRecord::Base
 
   validates_presence_of :description, :start_date, :end_date, :price, :tag_line
 
+  def taxable_for_user?(user)
+    distance = (user.home_year - start_date.year).abs
+    distance > 100
+  end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe Trip do
+
+  describe "taxes" do
+    let(:trip) { FactoryGirl.build(:trip,
+        :start_date => "Jan 22, 1971", :price => 100) }
+    let(:user) { FactoryGirl.build(:user) }
+
+    it "knows that there is no tax on nearby trips" do
+      user.home_year = 1971
+      expect(trip).not_to be_taxable_for_user(user)
+    end
+
+    it "knows there is a tax on far away trips" do
+      user.home_year = 2100
+      expect(trip).to be_taxable_for_user(user)
+    end
+  end
 end
